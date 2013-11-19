599a183588fb82af7b60e5dc136b0c7ab7a718f9
Oleg N. Cher <allot@bk.ru>
2013-11-19 10:40:41 +0200

    Win32: Ren '64' => 'x64'.

--- Win32/Bin/build64.bat
+++ Win32/Bin/build64.bat
@@ -6,5 +6,5 @@
 %tcc% ..\Lib\DW2Rsrc.c DW2Config.c SemiAction.c DWoods2.c -D_WINGUI -I "." -I %WinDevLib% -I %Lib% %WinDevLib%\WinDev64.a %WinDevLib%\Sdl\SdlLib64.a %WinDevBin%\tcc\lib\SDL.def -o DWoods2.exe
 @IF errorlevel 1 PAUSE
 
-@IF EXIST DWoods2.exe MOVE DWoods2.exe ..\64
-@START ..\64\DWoods2.exe
+@IF EXIST DWoods2.exe MOVE DWoods2.exe ..\x64
+@START ..\x64\DWoods2.exe
