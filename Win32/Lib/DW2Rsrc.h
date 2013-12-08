813f756aef5213dbe2e874a99e08ace723a7a60d
Oleg N. Cher <allot@bk.ru>
2013-12-08 23:47:28 +0200

    * Win32/Lib/DW2Rsrc

--- Win32/Lib/DW2Rsrc.h
+++ Win32/Lib/DW2Rsrc.h
@@ -6,7 +6,8 @@
 #include "SYSTEM.h"
 
 
-import SYSTEM_BYTE DW2Rsrc_CornerLeftUp[8], DW2Rsrc_CornerRightUp[8], DW2Rsrc_CornerLeftDown[8], DW2Rsrc_CornerRightDown[8], DW2Rsrc_HorizontalLine[8], DW2Rsrc_VerticalLine[8];
+import SYSTEM_BYTE DW2Rsrc_CornerLeftUp[8], DW2Rsrc_CornerRightUp[8], DW2Rsrc_CornerLeftDown[8], DW2Rsrc_CornerRightDown[8], DW2Rsrc_HorizontalLine[8], DW2Rsrc_VerticalLine[8], DW2Rsrc_CornerLeftUp2[8], DW2Rsrc_CornerRightUp2[8], DW2Rsrc_CornerLeftDown2[8], DW2Rsrc_CornerRightDown2[8], DW2Rsrc_VerticalLine2[8], DW2Rsrc_VerticalLine3[8], DW2Rsrc_Face[8], DW2Rsrc_Tree[8], DW2Rsrc_Bush[8], DW2Rsrc_Wall[8], DW2Rsrc_Door[8], DW2Rsrc_Bow[8], DW2Rsrc_Arrow[8], DW2Rsrc_Potion[8], DW2Rsrc_Key[8], DW2Rsrc_Gold[8], DW2Rsrc_Jewel[8], DW2Rsrc_Firewall[8], DW2Rsrc_Trap[8], DW2Rsrc_Orc[8], DW2Rsrc_Troll[8], DW2Rsrc_RingOfFire[8];
+import SYSTEM_BYTE DW2Rsrc_HorizontalLine2[16], DW2Rsrc_HorizontalLine3[16];
 
 
 #define DW2Rsrc__init()
